816175ab3142a99960d88a42fee5187e75f55e41
MirkoCuccurullo <90070321+MirkoCuccurullo@users.noreply.github.com>
2022-05-14 13:51:18 +0200

    employee logic

---
+++ RosLogic/EmployeeLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RosModel;
+using RosDAL;
+
+namespace RosLogic
+{
+    public class EmployeeLogic
+    {
+        EmployeeDAO employeedb = new EmployeeDAO();
+        public Employee GetEmployeeById(int id)
+        {
+            return employeedb.GetEmployeeById(id);
+        }
+
+        public Employee GetLastEmployeeID()
+        {
+            return employeedb.GetLastEmployeeID();
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return employeedb.GetAllEmployees();
+        }
+
+        public Employee GetEmployeeByUsername(string username)
+        {
+            return employeedb.GetEmployeeByUsername(username);
+        }
+
+        public void Add(Employee employee)
+        {
+            employeedb.Add(employee);
+        }
+    }
+}
