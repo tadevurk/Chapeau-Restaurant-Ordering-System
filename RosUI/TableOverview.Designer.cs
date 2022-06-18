d9db95bef5f9e100dd8a92bceefa8f1162eb3050
659045 <659045@student.inholland.nl>
2022-06-18 19:48:31 +0200

    small fix

--- RosUI/TableOverview.Designer.cs
+++ RosUI/TableOverview.Designer.cs
@@ -30,8 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.Timer tmrTimer;
-            this.t2DrinkIcon = new System.Windows.Forms.PictureBox();
-            this.t2DishIcon = new System.Windows.Forms.PictureBox();
             this.btnMoreInfo = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -51,19 +49,7 @@
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.lblDishOrder = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox8 = new System.Windows.Forms.PictureBox();
-            this.pictureBox9 = new System.Windows.Forms.PictureBox();
             tmrTimer = new System.Windows.Forms.Timer(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.t2DrinkIcon)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.t2DishIcon)).BeginInit();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -72,8 +58,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             this.SuspendLayout();
             // 
             // tmrTimer
@@ -81,28 +65,6 @@
             tmrTimer.Enabled = true;
             tmrTimer.Interval = 60000;
             tmrTimer.Tick += new System.EventHandler(this.tmrTimer_Tick);
-            // 
-            // t2DrinkIcon
-            // 
-            this.t2DrinkIcon.BackColor = System.Drawing.Color.LightGreen;
-            this.t2DrinkIcon.Image = global::RosUI.Properties.Resources.icons8_martini_glass_32;
-            this.t2DrinkIcon.Location = new System.Drawing.Point(116, 334);
-            this.t2DrinkIcon.Name = "t2DrinkIcon";
-            this.t2DrinkIcon.Size = new System.Drawing.Size(32, 28);
-            this.t2DrinkIcon.TabIndex = 23;
-            this.t2DrinkIcon.TabStop = false;
-            this.t2DrinkIcon.Visible = false;
-            // 
-            // t2DishIcon
-            // 
-            this.t2DishIcon.BackColor = System.Drawing.Color.LightGreen;
-            this.t2DishIcon.Image = global::RosUI.Properties.Resources.icons8_dinner_32;
-            this.t2DishIcon.Location = new System.Drawing.Point(48, 463);
-            this.t2DishIcon.Name = "t2DishIcon";
-            this.t2DishIcon.Size = new System.Drawing.Size(32, 32);
-            this.t2DishIcon.TabIndex = 24;
-            this.t2DishIcon.TabStop = false;
-            this.t2DishIcon.Visible = false;
             // 
             // btnMoreInfo
             // 
@@ -283,132 +245,11 @@
             this.lblDishOrder.TabIndex = 59;
             this.lblDishOrder.Text = "Dish Order";
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.LightGreen;
-            this.button1.Location = new System.Drawing.Point(284, 164);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 100);
-            this.button1.TabIndex = 60;
-            this.button1.Text = "1";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Visible = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.LightGreen;
-            this.button2.Location = new System.Drawing.Point(284, 270);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(180, 100);
-            this.button2.TabIndex = 61;
-            this.button2.Text = "2";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Visible = false;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.LightGreen;
-            this.button3.Location = new System.Drawing.Point(284, 376);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(180, 100);
-            this.button3.TabIndex = 62;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Visible = false;
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.LightGreen;
-            this.button4.Location = new System.Drawing.Point(284, 482);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(180, 100);
-            this.button4.TabIndex = 63;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = false;
-            this.button4.Visible = false;
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.Color.LightGreen;
-            this.button5.Location = new System.Drawing.Point(19, 164);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(180, 100);
-            this.button5.TabIndex = 64;
-            this.button5.Text = "5";
-            this.button5.UseVisualStyleBackColor = false;
-            this.button5.Visible = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(58, 334);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 25);
-            this.label1.TabIndex = 66;
-            this.label1.Text = "1";
-            this.label1.Visible = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(58, 376);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(22, 25);
-            this.label2.TabIndex = 67;
-            this.label2.Text = "2";
-            this.label2.Visible = false;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(116, 416);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(17, 20);
-            this.label3.TabIndex = 68;
-            this.label3.Text = "3";
-            this.label3.Visible = false;
-            // 
-            // pictureBox8
-            // 
-            this.pictureBox8.BackColor = System.Drawing.Color.LightGreen;
-            this.pictureBox8.Image = global::RosUI.Properties.Resources.icons8_dinner_32;
-            this.pictureBox8.Location = new System.Drawing.Point(167, 313);
-            this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(32, 32);
-            this.pictureBox8.TabIndex = 69;
-            this.pictureBox8.TabStop = false;
-            this.pictureBox8.Visible = false;
-            // 
-            // pictureBox9
-            // 
-            this.pictureBox9.BackColor = System.Drawing.Color.LightGreen;
-            this.pictureBox9.Image = global::RosUI.Properties.Resources.icons8_martini_glass_32;
-            this.pictureBox9.Location = new System.Drawing.Point(131, 232);
-            this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(32, 28);
-            this.pictureBox9.TabIndex = 70;
-            this.pictureBox9.TabStop = false;
-            this.pictureBox9.Visible = false;
-            // 
             // TableOverview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(482, 753);
-            this.Controls.Add(this.pictureBox9);
-            this.Controls.Add(this.pictureBox8);
-            this.Controls.Add(this.t2DishIcon);
-            this.Controls.Add(this.t2DrinkIcon);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblDishOrder);
             this.Controls.Add(this.pictureBox7);
             this.Controls.Add(this.pictureBox6);
@@ -430,8 +271,6 @@
             this.Name = "TableOverview";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TableOverview";
-            ((System.ComponentModel.ISupportInitialize)(this.t2DrinkIcon)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.t2DishIcon)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -441,16 +280,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox t2DrinkIcon;
-        private System.Windows.Forms.PictureBox t2DishIcon;
         private System.Windows.Forms.Button btnMoreInfo;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
@@ -470,15 +305,5 @@
         private System.Windows.Forms.PictureBox pictureBox6;
         private System.Windows.Forms.PictureBox pictureBox7;
         private System.Windows.Forms.Label lblDishOrder;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.PictureBox pictureBox8;
-        private System.Windows.Forms.PictureBox pictureBox9;
     }
 }
